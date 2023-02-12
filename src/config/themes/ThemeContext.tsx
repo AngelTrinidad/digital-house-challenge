cd9e74d8720adde049c968c36b85700c03dd6bfe
Angel Trinidad <96trinidad@gmail.com>
2023-02-11 23:16:56 -0300

    feat: setup theme provider

---
+++ src/config/themes/ThemeContext.tsx
@@ -0,0 +1,46 @@
+import React, {
+  createContext,
+  FC,
+  PropsWithChildren,
+  useCallback,
+  useMemo,
+  useState,
+} from 'react';
+
+import {Theme, ThemeContextValue, ThemeOption} from './interface';
+import {basicTheme} from './values/basicTheme';
+
+export const ThemeContext = createContext<ThemeContextValue>({
+  theme: basicTheme,
+  changeTheme: () => null,
+});
+
+export const ThemeProvider: FC<PropsWithChildren> = ({children}) => {
+  const [theme, setTheme] = useState(basicTheme);
+
+  const handleChangeTheme = useCallback((value: ThemeOption) => {
+    let newTheme: Theme;
+
+    switch (value) {
+      case ThemeOption.Basic:
+      default:
+        newTheme = basicTheme;
+    }
+
+    setTheme(newTheme);
+  }, []);
+
+  const contextValue = useMemo(
+    () => ({
+      theme,
+      changeTheme: handleChangeTheme,
+    }),
+    [theme, handleChangeTheme],
+  );
+
+  return (
+    <ThemeContext.Provider value={contextValue}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
---
+++ src/config/themes/values/basicTheme.ts
@@ -0,0 +1,17 @@
+import {Theme} from '../interface';
+
+export const basicTheme: Theme = {
+  colors: {
+    primary: '#334FFA',
+    secondary: '#CFD6FF',
+    success: '#00B833',
+    error: '#FF0000',
+    screen: {
+      primary: '#F8F8F8',
+    },
+    text: {
+      primary: 'black',
+      secondary: '#9B9898',
+    },
+  },
+};
